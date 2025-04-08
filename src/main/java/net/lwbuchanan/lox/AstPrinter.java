b18b3135682517a3375e62ef06aedc5666bbaf87
Luke Buchanan <luke1020will@gmail.com>
2025-04-08 16:17:59 -0400

    Ast generator and wip parser

---
+++ src/main/java/net/lwbuchanan/lox/AstPrinter.java
@@ -0,0 +1,47 @@
+package net.lwbuchanan.lox;
+
+import net.lwbuchanan.lox.Expr.Binary;
+import net.lwbuchanan.lox.Expr.Grouping;
+import net.lwbuchanan.lox.Expr.Literal;
+import net.lwbuchanan.lox.Expr.Unary;
+
+class AstPrinter implements Expr.Visitor<String> {
+
+    String print(Expr expr) {
+        return expr.accept(this);
+    }
+
+    private String parenthesize(String name, Expr... exprs) {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("(").append(name);
+        for (Expr expr : exprs) {
+            builder.append(" ");
+            builder.append(expr.accept(this));
+        }
+        builder.append(")");
+
+        return builder.toString();
+    }
+
+    @Override
+    public String visitBinaryExpr(Binary expr) {
+        return parenthesize(expr.operator.lexeme, expr.left, expr.right);
+    }
+
+    @Override
+    public String visitGroupingExpr(Grouping expr) {
+        return parenthesize("group", expr.expression);
+    }
+
+    @Override
+    public String visitLiteralExpr(Literal expr) {
+        return (expr.value == null) ? "nil" : expr.value.toString();
+    }
+
+    @Override
+    public String visitUnaryExpr(Unary expr) {
+        return parenthesize(expr.operator.lexeme, expr.right);
+    }
+
+}
