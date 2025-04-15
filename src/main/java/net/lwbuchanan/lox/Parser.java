b2fe1d9d23fa94d31d6f2da67397cf3a20138341
Luke Buchanan <luke1020will@gmail.com>
2025-04-15 14:37:40 -0400

    stop complaining linter

--- src/main/java/net/lwbuchanan/lox/Parser.java
+++ src/main/java/net/lwbuchanan/lox/Parser.java
@@ -86,15 +86,16 @@ class Parser {
         return;
 
       switch (peek().type) {
-      case CLASS:
-      case FUN:
-      case VAR:
-      case FOR:
-      case IF:
-      case WHILE:
-      case PRINT:
-      case RETURN:
-        return;
+        case CLASS:
+        case FUN:
+        case VAR:
+        case FOR:
+        case IF:
+        case WHILE:
+        case PRINT:
+        case RETURN:
+          return;
+        default:
       }
 
       advance();
