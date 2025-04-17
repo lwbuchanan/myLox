dbfb593fb26e5f28e143a162cf3eeb88355eaf31
Luke Buchanan <luke1020will@gmail.com>
2025-04-17 13:40:11 -0400

    Semantic analysis w/ resolver

--- src/main/java/net/lwbuchanan/lox/Interpreter.java
+++ src/main/java/net/lwbuchanan/lox/Interpreter.java
@@ -1,11 +1,14 @@
 package net.lwbuchanan.lox;
 
 import java.util.List;
+import java.util.Map;
 import java.util.ArrayList;
+import java.util.HashMap;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
   final Environment globals = new Environment();
   private Environment environment = globals;
+  private final Map<Expr, Integer> locals = new HashMap<>();
 
   Interpreter() {
     globals.define("clock", new LoxCallable() {
@@ -68,6 +71,17 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
     }
   }
 
+  void resolve(Expr expr, int depth) {
+    locals.put(expr, depth);
+  }
+
+  private Object lookUpVariable(Token name, Expr expr) {
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      return environment.getAt(distance, name.lexeme);
+    } else return globals.get(name);
+  }
+
   private boolean isTruthy(Object object) {
     if (object == null)
       return false;
@@ -91,6 +105,8 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
       return;
     throw new RuntimeError(operator, "Operands must numbers");
   }
+
+  // --- Visitor Methods ---
 
   @Override
   public Object visitBinaryExpr(Expr.Binary expr) {
@@ -169,13 +185,20 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
   @Override
   public Object visitVariableExpr(Expr.Variable expr) {
-    return environment.get(expr.name);
+    return lookUpVariable(expr.name, expr);
   }
 
   @Override
   public Object visitAssignExpr(Expr.Assign expr) {
     Object value = evaluate(expr.value);
-    environment.assign(expr.name, value);
+
+    Integer distance = locals.get(expr);
+    if (distance != null) {
+      environment.assignAt(distance, expr.name, value);
+    } else {
+      globals.assign(expr.name, value);
+    }
+
     return value;
   }
 
